35d79f3f1227391423c3c77cfb314d451137f248
Pete Lewis <mygit@pjblewis.com>
2024-11-19 08:00:41 -0500

    Packaging script

--- tools/package.sh
+++ tools/package.sh
@@ -3,19 +3,31 @@ set -euo pipefail
 
 die() { echo "$*" >&2 ; exit 1 ; }
 
-[ -n "$MSYSTEM" ] || die "Use package.bat on Windows."
+[ -v MSYSTEM ] && die "Use package.bat on Windows."
 
-build_install_package() {
+build_install() {
     local flavor="$1"
 
     "tools/build.sh" --preset "$flavor" --configure
     "tools/build.sh" --preset "$flavor" --config Release
 
     "tools/build.sh" --preset "$flavor" --install --config Release --prefix "install/$flavor" --strip
-
-    # todo, zip it
 }
 
-build_install_package windows-mingw64
-build_install_package linux-x86_64
-build_install_package linux-aarch64
+package() {
+    local flavor="$1"
+    local version="$2"
+    tar czv -C "install/$flavor/bin" -f "inline-it-$version-$flavor.tgz" $3
+}
+
+build_install linux-x86_64
+build_install linux-aarch64
+build_install windows-mingw64
+
+# Get the version from the host binary
+version="$("install/linux-$(uname -m)/bin/inline-it" --version | awk '/.+ version: [0-9.]+/ { print $3 }')"
+[ -n "$version" ] || die "Failed to get version number"
+
+package linux-x86_64 "$version" inline-it
+package linux-aarch64 "$version" inline-it
+package windows-mingw64 "$version" inline-it.exe
