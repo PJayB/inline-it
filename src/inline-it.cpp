d2d41f0cd5cbc699456804073bf8c9452c246d1e
Pete Lewis <me@pjblewis.com>
2025-01-01 07:27:11 -0500

    Better list separation. Use span<> for byte data.

--- src/inline-it.cpp
+++ src/inline-it.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <fstream>
 #include <array>
+#include <span>
 #include <iomanip>
 #include <cstdint>
 
@@ -34,29 +35,51 @@ namespace {
     using output_stream = std::basic_ostream<char>;
     using input_stream = std::basic_istream<char>;
 
+    // Returns a separator appropriate for a byte's position within a row.
+    inline const char* list_separator(
+        size_t column_offset,
+        size_t columns)
+    {
+        if (column_offset % columns == 0) {
+            return ",\n";
+        }
+        else {
+            return ", ";
+        }
+    }
+
+    // Writes a comma-separated list of bytes as hex. The bytes are split at
+    // every 'columns' columns, except the last byte, which never has a
+    // trailing comma or new-line.
     void print_bytes(
         output_stream& ostream,
-        const std::string_view& chars,
+        const std::span<uint8_t>& chars,
         const std::string_view& prefix,
-        size_t columnOffset,
+        size_t bytes_written,
         size_t columns)
     {
         static constexpr char hex_chars[] = "0123456789abcdef";
 
-        for (size_t i = 0; i < chars.size(); ++i, ++columnOffset) {
+        // If we've already written some data, separate the new data
+        // with either a new line or a comma
+        if (bytes_written > 0) {
+            ostream << list_separator(bytes_written, columns);
+        }
+
+        for (size_t i = 0; i < chars.size(); ++i, ++bytes_written) {
             // If we're starting a new line, write the prefix
-            if (columnOffset % columns == 0) {
+            if (bytes_written % columns == 0) {
                 ostream << prefix;
             }
 
             // Write the byte
             uint8_t c = static_cast<uint8_t>(chars[i]);
             ostream << "0x" << hex_chars[(c & 0xf0) >> 4]
-                << hex_chars[c & 0xf] << ", ";
+                << hex_chars[c & 0xf];
 
-            // If we're terminating a line, write a newline
-            if ((columnOffset + 1) % columns == 0) {
-                ostream << "\n";
+            // Write the separator for all but the last byte
+            if ((i + 1) < chars.size()) {
+                ostream << list_separator(bytes_written + 1, columns);
             }
         }
     }
@@ -65,25 +88,29 @@ namespace {
         output_stream& ostream,
         input_stream& istream,
         const std::string_view& linePrefix,
-        const std::string_view& streamSuffix)
+        bool null_terminate)
     {
-        char batch[2048]; // todo audit batch size
+        std::array<uint8_t, 2048> batch; // todo audit batch size
         constexpr size_t columns = 8; // must be factor of batch size
-        size_t written = 0;
+        size_t bytes_written = 0;
         while(istream.good()) {
-            istream.read(batch, std::size(batch));
+            istream.read(reinterpret_cast<char*>(batch.data()), batch.size());
             auto r = istream.gcount();
             if (r > 0) {
-                print_bytes(ostream, std::string_view(batch, r),
-                    linePrefix, written % columns, columns);
-                written += r;
+                // Write this block of bytes
+                print_bytes(ostream, std::span {
+                        batch.begin(),
+                        batch.begin() + r
+                    }, linePrefix, bytes_written, columns);
+                bytes_written += r;
             }
         }
         if (!istream.eof()) {
             istream.exceptions(std::ofstream::failbit);
         }
-        if (streamSuffix.size() > 0) {
-            print_bytes(ostream, streamSuffix, linePrefix, written % columns,
+        if (null_terminate) {
+            std::array<uint8_t, 1> null { 0 };
+            print_bytes(ostream, std::span { null }, linePrefix, bytes_written,
                 columns);
         }
     }
@@ -97,26 +124,25 @@ namespace {
         bool null_terminate)
     {
         // If not doing bare output, indent the values
-        std::string_view linePrefix { bare ? "" : "\t" };
-        std::string_view streamSuffix { null_terminate ? "\0" : "" };
+        std::string_view line_prefix { bare ? "" : "\t" };
 
         if (input_files.empty()) {
-            dump_hex(ostream, std::cin, linePrefix, streamSuffix);
+            dump_hex(ostream, std::cin, line_prefix, null_terminate);
         }
         else {
             for (auto const& filename : input_files) {
                 std::ifstream f(filename.c_str(), std::ios::binary);
 
                 if (bare) {
-                    dump_hex(ostream, f, linePrefix, streamSuffix);
+                    dump_hex(ostream, f, line_prefix, null_terminate);
                 }
                 else {
                     // Generate identifier from filename
                     ostream << type_mods << " "
                         << filename_to_identifier(filename, id_prefix)
                         << "[] = {" << std::endl;
-                    dump_hex(ostream, f, linePrefix, streamSuffix);
-                    ostream << "};\n";
+                    dump_hex(ostream, f, line_prefix, null_terminate);
+                    ostream << "\n};\n";
                 }
             }
         }
